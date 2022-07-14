fdd86c764dc688745e1c7eee9b47399c6182b0eb
npqHqJKee <npqHqJKee@gmail.com>
2022-07-14 14:41:12 +0300

    init

---
+++ src/components/MarketPlaceGoods.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>Here will be goods :)</div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+export default class MarketPlaceGoods extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
+import MarketPlaceGoods from "./components/MarketPlaceGoods.vue";
 
 Vue.config.productionTip = false;
+Vue.component("market-place-goods", MarketPlaceGoods);
 
 new Vue({
   render: (h) => h(App),
